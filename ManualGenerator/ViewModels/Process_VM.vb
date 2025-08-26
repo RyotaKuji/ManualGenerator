c65d414bf923628cce55ad2fa1dd77dae3aae4d9
RyotaKuji <kdt.nmk2@gmail.com>
2025-08-26 18:44:41 +0900

    Removed SegmentButton

--- ManualGenerator/ViewModels/Process_VM.vb
+++ ManualGenerator/ViewModels/Process_VM.vb
@@ -9,25 +9,9 @@ Public Class Process_VM
 	Public Property DescriptionXaml As String
 	Public Property DescriptionHtml As String
 	Public Property ImagePath As String
-	Private _isHeadingVar As Boolean
-	Public Property IsHeading As Boolean
-		Get
-			Return _isHeadingVar
-		End Get
-		Set(value As Boolean)
-			SetProperty(_isHeadingVar, value)
-		End Set
-	End Property
-
-	<JsonIgnore>
-	Public Const HeadingFlag = "Heading"
-	<JsonIgnore>
-	Public Const ProcessFlag = "Process"
 
 	<JsonIgnore>
 	Public Property RemoveCommand As RelayCommand
-	<JsonIgnore>
-	Public Property SelectionChangedCommand As RelayCommand(Of String)
 
 	Private _isLastItem As Boolean
 	<JsonIgnore>
@@ -39,14 +23,5 @@ Public Class Process_VM
 			SetProperty(_isLastItem, value)
 		End Set
 	End Property
-
-	Public Sub New()
-		SelectionChangedCommand = New RelayCommand(Of String)(AddressOf SelectionChanged)
-		SelectionChanged(ProcessFlag)
-	End Sub
-
-	Private Sub SelectionChanged(contentType As String)
-		IsHeading = contentType = HeadingFlag
-	End Sub
 
 End Class
