5191f22d7fd0a3f93be8920488f332c9b39ae148
RyotaKuji <kdt.nmk2@gmail.com>
2025-08-27 23:26:18 +0900

    Added XamlToHtmlConverter proj

---
+++ XamlToHtmlConverter/HtmlEncodedTextWriter.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Xml;
+
+namespace XamlToHtmlConverter
+{
+	public class HtmlEncodedTextWriter : XmlTextWriter
+	{
+		public HtmlEncodedTextWriter(TextWriter w) : base(w) { }
+
+		#region Overrides of XmlTextWriter
+
+		/// <inheritdoc />
+		public override void WriteString(string text)
+		{
+			text = WebUtility.HtmlEncode(text);
+			WriteRaw(text);
+		}
+
+		#endregion
+	}
+}
---
+++ XamlToHtmlConverter/ReadMe.txt
@@ -0,0 +1,2 @@
+﻿It was copied from Microsoft's Demo
+https://github.com/microsoft/WPF-Samples/tree/main/Sample%20Applications/HtmlToXamlDemo
