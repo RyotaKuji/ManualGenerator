47d32e23ba68ac381f704e3a11505d2bfd1e8ab1
RyotaKuji <kdt.nmk2@gmail.com>
2025-09-05 07:54:32 +0900

    Added Custom Scrollbar

--- ManualGenerator/Views/EditorPage.xaml.vb
+++ ManualGenerator/Views/EditorPage.xaml.vb
@@ -9,4 +9,12 @@
 		InitializeComponent()
 		DataContext = vm
 	End Sub
+
+	Private Sub ListBox_PreviewMouseWheel(sender As Object, e As MouseWheelEventArgs)
+		e.Handled = True
+		Dim eventArg = New MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+		eventArg.RoutedEvent = UIElement.MouseWheelEvent
+		eventArg.Source = sender
+		ScrollViewer.RaiseEvent(eventArg)
+	End Sub
 End Class
