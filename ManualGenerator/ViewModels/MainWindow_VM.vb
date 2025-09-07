288bd1352c987b7e144d5479979d2c824dd7079f
RyotaKuji <kdt.nmk2@gmail.com>
2025-09-07 16:07:32 +0900

    Added Placeholder

---
+++ ManualGenerator/ViewModels/MainWindow_VM.vb
@@ -0,0 +1,21 @@
+﻿Imports CommunityToolkit.Mvvm.ComponentModel
+
+Public Class MainWindow_VM : Inherits ObservableObject
+
+	Private _editorPage_VM As EditorPage_VM
+	Public Property EditorPage_VM As EditorPage_VM
+		Get
+			Return _editorPage_VM
+		End Get
+		Set(value As EditorPage_VM)
+			SetProperty(_editorPage_VM, value)
+			OnPropertyChanged(NameOf(IsEditorMode))
+		End Set
+	End Property
+
+	Public ReadOnly Property IsEditorMode As Boolean
+		Get
+			Return EditorPage_VM IsNot Nothing
+		End Get
+	End Property
+End Class
--- ManualGenerator/Views/MainWindow.xaml.vb
+++ ManualGenerator/Views/MainWindow.xaml.vb
@@ -1,17 +1,27 @@
 ﻿Class MainWindow
 
+	Private ReadOnly Property VM As MainWindow_VM
+		Get
+			Return TryCast(DataContext, MainWindow_VM)
+		End Get
+	End Property
+
 	Public Sub New()
 		InitializeComponent()
+		DataContext = New MainWindow_VM()
+
 		NavigateToListPage()
 	End Sub
 
 	Public Sub NavigateToListPage()
+		VM.EditorPage_VM = Nothing
 		Frame.Navigate(New Uri("Views/ListPage.xaml", UriKind.Relative))
 	End Sub
 
 	Public Sub NavigateToEditorPage(id As String)
-		Dim vm As New EditorPage_VM(id)
-		Frame.Navigate(New EditorPage(vm))
-	End Sub
+		Dim pageVM As New EditorPage_VM(id)
+		VM.EditorPage_VM = pageVM
 
+		Frame.Navigate(New EditorPage(pageVM))
+	End Sub
 End Class
